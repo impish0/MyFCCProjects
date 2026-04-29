858f7cab2219501703e19dbcb5c4a1fed62e84e4
impish0 <admin@dustinhogate.com>
2026-04-29 12:20:21 -0800

    prettier

--- Javascript/mathbot.js
+++ Javascript/mathbot.js
@@ -3,7 +3,9 @@ const greeting = `Hi there! My name is ${botName} and I am here to teach you abo
 
 console.log(greeting);
 
-console.log("The Math.random() method returns a pseudo random number greater than or equal to 0 and less than 1.");
+console.log(
+  "The Math.random() method returns a pseudo random number greater than or equal to 0 and less than 1.",
+);
 
 const randomNum = Math.random();
 console.log(randomNum);
@@ -16,7 +18,9 @@ const max = 100;
 const randomNum2 = Math.random() * (max - min) + min;
 console.log(randomNum2);
 
-console.log("The Math.floor() method rounds the value down to the nearest whole integer.");
+console.log(
+  "The Math.floor() method rounds the value down to the nearest whole integer.",
+);
 
 const numRoundedDown = Math.floor(6.7);
 console.log(numRoundedDown);
@@ -26,13 +30,15 @@ console.log("Now, generate a random integer between two values.");
 const randomInt = Math.floor(Math.random() * (max - min) + min);
 console.log(randomInt);
 
-console.log("The Math.ceil() method rounds the value up to the nearest whole integer.");
+console.log(
+  "The Math.ceil() method rounds the value up to the nearest whole integer.",
+);
 
 const numRoundedUp = Math.ceil(3.2);
 console.log(numRoundedUp);
 
 console.log(
-  "The Math.round() method rounds the value to the nearest whole integer."
+  "The Math.round() method rounds the value to the nearest whole integer.",
 );
 
 const numRounded = Math.round(2.7);
@@ -40,7 +46,9 @@ console.log(numRounded);
 const numRounded2 = Math.round(11.2);
 console.log(numRounded2);
 
-console.log("The Math.max() and Math.min() methods are used to get the maximum and minimum number from a range.");
+console.log(
+  "The Math.max() and Math.min() methods are used to get the maximum and minimum number from a range.",
+);
 
 const maxNum = Math.max(3, 125, 55, 24);
 console.log(maxNum);
